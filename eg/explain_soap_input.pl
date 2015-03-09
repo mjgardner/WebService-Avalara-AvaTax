820f2665c9633fe581110416ba06695204661e55
Mark Gardner <mjgardner@cpan.org>
2015-03-09 12:11:30 -0400

    add example script and current output showing how to retrieve explanation of SOAP inputs

---
+++ eg/explain_soap_input.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Const::Fast;
+use WebService::Avalara::AvaTax;
+
+const my @AVALARA_ENV => qw(username password);
+const my %SOAP_PARAMS => ( service => 'TaxSvc', port => 'TaxSvcSoap' );
+
+my $avatax
+    = WebService::Avalara::AvaTax->new( map { ( $_ => $ENV{"AVALARA_\U$_"} ) }
+        @AVALARA_ENV );
+
+say $avatax->wsdl->explain( $_, PERL => 'INPUT', recurse => 1, %SOAP_PARAMS )
+    for map { $_->name } $avatax->wsdl->operations;
